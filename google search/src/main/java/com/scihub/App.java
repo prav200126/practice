48669cb4daf16379adacd81baab0ceb4b3f3bb6f
prav200126 <p54165600@gmail.com>
2024-11-11 09:41:30 +0530

    firaat post

---
+++ google search/src/main/java/com/scihub/App.java
@@ -0,0 +1,11 @@
+package com.scihub;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("if u  want  to search more  information pls reach me out");
+    }
+}
