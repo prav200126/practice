7ade4f9342f4900fcc6b5aaa34bb995d78d82b44
prav200126 <p54165600@gmail.com>
2024-11-11 09:42:52 +0530

    doubt

--- google search/src/main/java/com/scihub/App.java
+++ google search/src/main/java/com/scihub/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("if u  want  to search more  information pls reach me out");
+        hot to search the post pls tell  me brief about it
     }
 }
